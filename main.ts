bb80937a4c81842614175ac15ed0a1c139bb52d1
687472 <127861540+687472@users.noreply.github.com>
2023-04-24 12:00:03 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,4 @@
+let mySprite = sprites.create(assets.image`tony`, SpriteKind.Player)
+controller.moveSprite(mySprite)
+tiles.setCurrentTilemap(tilemap`trhhwhwhwhwbe`)
+scene.cameraFollowSprite(mySprite)
